4ffda46bde2895f9b12492c56f8d0ba83a902487
katr1na <katrinango3388@gmail.com>
2025-12-14 17:40:05 -0700

    Fix unused env in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -13,14 +13,22 @@
 //   }
 // })
 
-import { defineConfig, loadEnv } from 'vite'
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig(({ mode }) => {
+//   // Load ONLY VITE_ variables
+//   const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+//   return {
+//     plugins: [react()],
+//   }
+// })
+
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load ONLY VITE_ variables
-  const env = loadEnv(mode, process.cwd(), 'VITE_')
-
-  return {
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
 })
+
